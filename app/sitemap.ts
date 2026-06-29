98429afc2f31dd041385f27b337bb2d44ddbc850
mateushahndev <mateushahn333@gmail.com>
2026-06-29 11:41:52 -0300

    feat: atualizando sitemap com todas as páginas do site

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,13 +2,32 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://mateushahn.com'
+  const currentDate = new Date()
 
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: currentDate,
       changeFrequency: 'monthly',
-      priority: 1,
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/contato`,
+      lastModified: currentDate,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/politica-privacidade`,
+      lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+    {
+      url: `${baseUrl}/termos-uso`,
+      lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 0.3,
     },
   ]
 }
